7784516d7ca1b4c88a24db757a30c728cb805868
Rebecca Jackson <zimbecs@gmail.com>
2019-02-28 11:13:55 +1300

    ticket info displaying on localhost

--- client/containers/AllTickets.jsx
+++ client/containers/AllTickets.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-class AllTickets extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+export class AllTickets extends React.Component {
 
-        }
-    }
 
-    render() {
-        return (
-            <div>AllTickets</div>
-        )
-    }
+  render() {
+    return (
+      <div className='tickets-container'>
+        <h1>Tickets</h1>
+        <div className='tickets-table'>
+          <div className='table-header'>
+            sortby header
+          </div>
+          <div>
+            map through info here
+          </div>
+
+        </div>
+        <div className="keys-container">
+          <h4>import keys component here</h4>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default AllTickets
+function mapStateToProps({ allTickets }){
+  return {
+    allTickets
+  }
+}
+
+export default connect(mapStateToProps)(AllTickets)
