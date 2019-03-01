6e3f3c0237e4a58e304ff5c685e9842699491e14
Rebecca Jackson <zimbecs@gmail.com>
2019-03-01 21:44:51 +1300

    add a connection file to handle testDb conditions

--- server/db/locations.js
+++ server/db/locations.js
@@ -1,17 +1,17 @@
-const knex = require ('knex')
-const config = require ('../../knexfile').development
-const db = knex(config)
+const connection = require('./connection')
 
-
-function getLocations() {
+function getLocations(testDb) {
+    const db = testDb || connection
     return db('locations')
 }
 
-function getLocationById(id) {
+function getLocationById(id, testDb) {
+    const db = testDb || connection
     return db('locations').where('id', id).select()
 }
 
-function getLocationsByParent(id) {
+function getLocationsByParent(id, testDb) {
+    const db = testDb || connection
     return db('locations').where('parent_id', id).select()
 }
 
