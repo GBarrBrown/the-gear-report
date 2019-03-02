22ce24651cfd8191a1ff3a41ba3733b7d27b686a
Rebecca Jackson <zimbecs@gmail.com>
2019-03-02 14:47:24 +1300

    auth with facebook setup

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -1,18 +1,62 @@
 import React from 'react'
+import FacebookLogin from 'react-facebook-login'
+
+
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+    state = {
+        isLoggedIn: false,
+        userID: "",
+        name: "",
+        email: "",
+        picture: ""
+      };
 
+      responseFacebook = (response) => {
+        console.log(response);
+        this.setState({
+          name: response.name,
+          picture: response.picture.data.url,
+          isLoggedIn: true
+        })
+        // now to send that info to our db and route to last address in history
+      }
+
+      componentClicked = () => console.log("clicked"); //loading action here
+
+      render() {
+        let fbContent;
+    
+        if (this.state.isLoggedIn) {
+          fbContent = (
+            <div
+              style={{
+                width: "400px",
+                margin: "auto",
+                background: "#f4f4f4",
+                padding: "20px"
+              }}
+            >
+              <img src={this.state.picture} alt={this.state.name} />
+              <h2>Welcome {this.state.name}</h2>
+              
+            </div>
+          );
+        } else {
+          fbContent = (
+            <FacebookLogin
+              appId="249678739247240"
+              autoLoad={true}
+              fields="name,email,picture"
+              onClick={this.componentClicked}
+              callback={this.responseFacebook}
+            />
+          );
         }
+    
+        return <div>{fbContent}</div>;
+      }
     }
-
-    render() {
-        return (
-            <div>Login</div>
-        )
-    }
-}
 
 export default Login
+
