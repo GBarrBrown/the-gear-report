bced3c83b103901224d4c37d30b02f34e8437633
gbarrbrown <gbb7899@gmail.com>
2019-03-02 21:55:33 +1300

    moving header to a list

--- client/containers/Header.jsx
+++ client/containers/Header.jsx
@@ -7,42 +7,19 @@ class Header extends React.Component {
     const { auth } = this.props
     return (
       <div className="header">
-        <a href="/">
-          <div className="header-items">
-            Home
-          </div>
-        </a>
-        <a href="/tickets">
-          <div className="header-items">
-            Tickets
-          </div>
-        </a>
-        <a href="/faq">
-          <div className="header-items">
-            FAQ
-          </div>
-        </a>
-        <a href="/about">
-          <div className="header-items">
-            About
-          </div>
-        </a>
-        {auth.isAuthenticated ? 
-        ( <div className="header-items">
-            <h3>{auth.user.name}</h3>
-            <img src={auth.user.picture} alt="Profile pic"/>
-          </div>
-        ) : 
-        (
-          <a href="/login">
-            <div className="header-items">
-              Login {auth.errorMessage}
-            </div>
-          </a>
-        )
-      }
+        <ul>
+          <li className="header-items">Home</li>
+          <li className="header-items">Tickets</li>
+          <li className="header-items">FAQ</li>
+          <li className="header-items">About</li>
+          <li className="header-items">Login</li>
 
+        </ul>
       </div>
+      
+
+
+
     )
   }
 }
@@ -53,3 +30,42 @@ const mapStateToProps = ({auth}) => {
   }
 
     export default connect(mapStateToProps)(Header)
+
+
+          // <div className="header">
+      //   <a href="/">
+      //     <div className="header-items">
+      //       Home
+      //     </div>
+      //   </a>
+      //   <a href="/tickets">
+      //     <div className="header-items">
+      //       Tickets
+      //     </div>
+      //   </a>
+      //   <a href="/faq">
+      //     <div className="header-items">
+      //       FAQ
+      //     </div>
+      //   </a>
+      //   <a href="/about">
+      //     <div className="header-items">
+      //       About
+      //     </div>
+      //   </a>
+      //   {auth.isAuthenticated ? 
+      //   ( <div className="header-items-login">
+      //       <h3>{auth.user.name}</h3>
+      //       <img src={auth.user.picture} alt="Profile pic"/>
+      //     </div>
+      //   ) : 
+      //   (
+      //     <a href="/login">
+      //       <div className="header-items-login">
+      //         Login {auth.errorMessage}
+      //       </div>
+      //     </a>
+      //   )
+      // }
+
+      // </div>
