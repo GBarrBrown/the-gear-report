6eb2625f5958d6d9efd7d3b7f6e1a748fdd4e938
Richard Graham <richgrahamdev@gmail.com>
2019-03-03 15:01:44 +1300

    about to start jsx render

--- client/containers/Directory.jsx
+++ client/containers/Directory.jsx
@@ -32,16 +32,9 @@ const styles = theme => ({
   },
 });
 
-const testItem = [
-	{id: 2, title: 'test1'},
-	{id: 3, title: 'test2'},
-]
-
-
-
 class Directory extends React.Component {
   state = {
-		selectedOpen: true,
+		open: true,
 		selectedIndex: 1,
 	};
 	
@@ -66,111 +59,8 @@ class Directory extends React.Component {
 
     return (
         <div className='directory'>
-					<List
-        		component="nav"
-        		subheader={<ListSubheader component="div">Directory</ListSubheader>}
-        		className={classes.root}>
-						<ListItem button>
-          		<ListItemText inset primary="New Zealand" inset={true}/>
-        		</ListItem>
-
-								<ListItem button 
-													selected={this.state.selectedIndex === 1}
-													onClick={event => this.handleListItemClick(event, 1)}>
-										<ListItemIcon>
-											<UpIcon />
-										</ListItemIcon>
-              		<ListItemText inset secondary="North Island" />
-            		</ListItem>
-
-								<Collapse in={this.state.selectedOpen} timeout="auto" unmountOnExit>
-									<List component="div" disablePadding>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Northland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Auckland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Waikato" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Taupo" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Wellington" />
-        						</ListItem>
-									</List >
-								</Collapse>
-								
-            		<ListItem button
-													selected={this.state.selectedIndex === 2}
-													onClick={}>
-              		<ListItemText inset secondary="South  Island" />
-            		</ListItem>
-
-								<Collapse in={this.state.open} timeout="auto" unmountOnExit>
-									<List component="div" disablePadding>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Northland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Auckland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Waikato" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Taupo" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Wellington" />
-        						</ListItem>
-									</List >
-								</Collapse>
-
-								<ListItem button
-													selected={this.state.selectedIndex === 3}
-													onClick={}>
-              		<ListItemText inset secondary="South  Island" />
-            		</ListItem>
-
-								<Collapse in={this.state.open} timeout="auto" unmountOnExit>
-									<List component="div" disablePadding>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Northland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Auckland" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Waikato" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Taupo" />
-        						</ListItem>
-										<ListItem button className={classes.nested}>
-         							<ListItemText inset secondary="Wellington" />
-        						</ListItem>
-									</List >
-								</Collapse>
-
-
-
-
-
-
-
-						{/* <ListItem button
-											selected={this.state.selectedIndex === 1}
-											onClick={event => this.handleListItemClick(event, 1)}>
-								<ListItemIcon>
-									<UpIcon />
-								</ListItemIcon>
-								<ListItemText inset primary="North Island" />
-							</ListItem> */}
-			
-			</List>
+					{this.props.currentLocation && 
+					<h1>hi</h1>}
       </div>
     );
   }
--- holder.js
+++ holder.js
@@ -1,40 +1,48 @@
 <List
-        component="nav"
-        subheader={<ListSubheader component="div">Directory</ListSubheader>}
-        className={classes.root}
-      >
-        <ListItem button
-                	selected={this.state.selectedIndex === 1}
-									onClick={event => this.handleListItemClick(event, 1)}>
-          <ListItemIcon>
-            <UpIcon />
-          </ListItemIcon>
-          <ListItemText inset primary="North Island" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText inset primary="Northland" />
-        </ListItem>
-        <ListItem button onClick={this.handleClick} >
-          <ListItemText inset primary="Auckland" />
-          {this.state.open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemText inset primary="Auckland City" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemText inset primary="Waiheke Island" />
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItem button>
-          <ListItemText inset primary="Waikato" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText inset primary="Coromandel" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText inset primary="Wellington" />
-        </ListItem>
-      </List>
+        		component="nav"
+        		subheader={<ListSubheader component="div">Directory</ListSubheader>}
+        		className={classes.root}>
+						<ListItem button>
+          		<ListItemText inset primary="New Zealand" inset={true}/>
+        		</ListItem>
+
+								<ListItem button 
+													selected={this.state.selectedIndex === 1}
+													onClick={event => this.handleListItemClick(event, 1)}>
+										<ListItemIcon>
+											<UpIcon />
+										</ListItemIcon>
+              		<ListItemText inset secondary="North Island" />
+            		</ListItem>
+
+								<Collapse in={this.state.open} timeout="auto" unmountOnExit>
+									<List component="div" disablePadding>
+										<ListItem button className={classes.nested}>
+         							<ListItemText inset secondary="Northland" />
+        						</ListItem>
+										<ListItem button className={classes.nested}>
+         							<ListItemText inset secondary="Auckland" />
+        						</ListItem>
+										<ListItem button className={classes.nested}>
+         							<ListItemText inset secondary="Waikato" />
+        						</ListItem>
+										<ListItem button className={classes.nested}>
+         							<ListItemText inset secondary="Taupo" />
+        						</ListItem>
+										<ListItem button className={classes.nested}>
+         							<ListItemText inset secondary="Wellington" />
+        						</ListItem>
+									</List >
+								</Collapse>
+								
+            		<ListItem button
+													selected={this.state.selectedIndex === 2}>
+              		<ListItemText inset secondary="South  Island" />
+            		</ListItem>
+
+								<ListItem button
+													selected={this.state.selectedIndex === 3}>
+              		<ListItemText inset secondary="South  Island" />
+            		</ListItem>
+			
+			</List>
