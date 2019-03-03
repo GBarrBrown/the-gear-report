e97bce3fa2e2368fbbc49c1d0049902f5f1f04f2
Rebecca Jackson <zimbecs@gmail.com>
2019-03-03 16:43:26 +1300

    written db function to save new ticket

--- server/db/tickets.js
+++ server/db/tickets.js
@@ -4,38 +4,23 @@ const connection = require('./connection')
 
 function addTicket(user, title, description, severity, location, testDb){
   const db = testDb || connection
-  
-  //TODO get definition on data that is saved 
 
-  // return db('tickets')
-  // // .insert({
-  // //   title: title,
-  // //   description: description,
-  // //   severity: severity,
-  
-  // then(newticketId
-  // join_table
-  //   locations.map(object
-       
-  //     insert({t_id: newTickedId, l_id: object})
-  //   )
-  // )
-  // .insert()
-  
-  
-  // })
-  // .select('users.id').first().then(user => {
-  //   return db('garden')
-  //   .where('user_id', user.id)
-  //   .where('veg_id', veg.id).then((rows)=>{
-  //     if (rows.length===0) {
-  //       return db('garden').insert({veg_id: veg.id, user_id: user.id})
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   })
-  // })
+  return db('tickets')
+  .insert({
+    title: title,
+    description: description,
+    severity: severity
+  })
+  .then(newTicketId => {
+    location.map(id => {
+      if(typeof id === number)
+       return db('ticket_loc')
+       .insert({t_id: newTicketId, l_id: id})
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
 
 function getAllTickets(testDb) {
