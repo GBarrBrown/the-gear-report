bd1ad763faf867612d804ec0c7e1b7d38ea6fff2
gbarrbrown <gbb7899@gmail.com>
2019-03-05 17:11:17 +1300

    update tickets migrations with resolved col

--- migrations/20190228205533_tickets.js
+++ migrations/20190228205533_tickets.js
@@ -1,17 +1,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tickets', (table) => {
-      table.increments('id').primary()
-      table.string('title')
-          .notNullable();
-      table.text('description')
-          .notNullable();
-      table.integer('severity')
-          .notNullable();
-      table.string('img')
-      table.boolean('has_grant')
-          .defaultTo(false)
-      table.timestamps(true, true)
-      table.integer('user_id')
+    table.increments('id').primary()
+    table.string('title').notNullable();
+    table.text('description').notNullable();
+    table.integer('severity').notNullable();
+    table.string('img')
+    table.boolean('has_grant').defaultTo(false)
+    table.boolean('resolved').defaultTo(false)
+    table.timestamps(true, true)
+    table.integer('user_id')
   })
 };
 
