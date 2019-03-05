59c5115930d0b7f1b79d8174ceeef08e2af3ef3d
Rebecca Jackson <zimbecs@gmail.com>
2019-03-05 20:23:14 +1300

    updated card component with styles and classnames

--- client/containers/dispTop/Card.jsx
+++ client/containers/dispTop/Card.jsx
@@ -3,19 +3,14 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
     card: {
         width: '100',
-
-    },
-    media: {
-    //   height: 
     },
   };
 
@@ -24,7 +19,6 @@ const styles = {
     location.href=`/tickets/ticketId/${viewId}`
     : location.href=`/tickets/`
   }
-
   
   function MediaCard(props) {
     const { classes } = props;
@@ -61,14 +55,6 @@ const styles = {
           </CardContent>
           </div>
         </CardActionArea>
-        {/*4<CardActions>
-          <Button size="small" color="primary" onClick={event => { eventHandler(props.id) }}>
-            View Ticket
-          </Button>
-          <Button size="small" color="primary" onClick={event => { eventHandler() }}>
-            View All Tickets
-          </Button>
-        </CardActions> */}
       </Card>
       </div>
     );
