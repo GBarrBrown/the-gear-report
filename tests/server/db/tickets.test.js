8351c4df6c1b20ac057fba246116264cf3e5af1f
gbarrbrown <gbb7899@gmail.com>
2019-03-06 22:07:49 +1300

    tests - getTicketsByIds, getTicketCreator, getTopContributors

--- tests/server/db/tickets.test.js
+++ tests/server/db/tickets.test.js
@@ -26,6 +26,7 @@ test('GET gets all the tickets', () => {
   })
 })
 
+// -- getTicketById --
 test('GET ticket by id with existing ticket id', () => {
   let expected = 'Fallen Tree'
 
@@ -59,3 +60,67 @@ test('GET ticket by id passing it a string', () => {
     expect(actual).toEqual(expected)
   })
 })
+
+// -- getTicketsByIds
+test('GET tickets by ids with existing ids', () => {
+  let expected = ['Rockfall', 'Rock slide']
+  let ticketArr = [3,9]
+
+  return db.getTicketsByIds(ticketArr, testDb)
+  .then(res => {
+    let result = [res[0].title, res[1].title]
+    let actual = result
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('GET tickets by ids with 2 non-existent ids', () => {
+  let expected = []
+  let ticketArr = [0, 999999999999999]
+
+  return db.getTicketsByIds(ticketArr, testDb)
+  .then(res => {
+    let actual = res
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+test('GET tickets by ids with 1 non-existant and 1 existing id', () => {
+  let expected = 1
+  let ticketArr = [0, 3]
+
+  return db.getTicketsByIds(ticketArr, testDb)
+  .then(res => {
+    let actual = res.length
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+// -- getTicketCreator
+test('GET ticket creator with existing ticket id', () => {
+  let expected = 'Bob Blogs'
+  let creatorId = 1
+
+  return db.getTicketCreator(creatorId, testDb)
+  .then(res => {
+    let actual = res.name
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+// -- getTopContributors
+test('GET top contributors - should not return > 5 results', () => {
+  let expected = 5
+
+  return db.getTopContributors(testDb)
+  .then(res => {
+    let actual = res.length
+
+    expect(actual).toBeLessThanOrEqual(expected)
+  })
+})
+
