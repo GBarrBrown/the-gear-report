11745cb3d22c1d3b405f52175a710cc3c0dd1d5b
Richard Graham <richgrahamdev@gmail.com>
2019-03-07 00:49:40 +1300

    removed knex restrictions

--- migrations/20190228205533_tickets.js
+++ migrations/20190228205533_tickets.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tickets', (table) => {
     table.increments('id').primary()
-    table.string('title').notNullable();
+    table.string('title')
     table.text('description');
-    table.integer('severity').notNullable();
+    table.integer('severity')
     table.string('img')
     table.boolean('has_grant').defaultTo(false)
     table.boolean('resolved').defaultTo(false)
