babeb4fbc92b2f2c584207b9d8a4372091768c49
gbarrbrown <gbb7899@gmail.com>
2019-03-06 19:06:57 +1300

    fix spacing 4-2 spaces

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -11,64 +11,64 @@ import {getLocationsByParent} from '../api/local/locations'
 import {getTopContributors} from '../api/local/tickets'
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
-        this.renderCondition = this.renderCondition.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
     }
+    this.renderCondition = this.renderCondition.bind(this)
+  }
 
-    componentDidMount() {
-        this.props.updateCurrentLocation(this.props.match.params.id)
-        this.props.getTopContributors()
+  componentDidMount() {
+    this.props.updateCurrentLocation(this.props.match.params.id)
+    this.props.getTopContributors()
+  }
+
+  componentDidUpdate() {
+    this.props.getLocationsByParent(this.props.currentLocation)   
+  }
+
+  renderCondition(type) {
+    switch(type) {
+      case 'crag':
+        return <Info props={this.props}/>
+      case 'wall':
+        return <Info props={this.props}/>
+      case 'route':
+        return <Info props={this.props}/>
+      case 'cliff':
+        return <Info props={this.props}/>
+      default:
+        return <Globe props={this.props}/>
     }
+  }
 
-    componentDidUpdate() {
-        this.props.getLocationsByParent(this.props.currentLocation)   
-    }
+  
 
-    renderCondition(type) {
-        switch(type) {
-          case 'crag':
-            return <Info props={this.props}/>
-          case 'wall':
-            return <Info props={this.props}/>
-          case 'route':
-            return <Info props={this.props}/>
-          case 'cliff':
-            return <Info props={this.props}/>
-          default:
-            return <Globe props={this.props}/>
-        }
-      }
-
-    
-
-    render() {
-        return (
-            <div className="dashboard-container">
-              <Directory />
-              <div className='globe'>
-              {this.props.loadLocationById.length > 0 && 
-                this.props.loadParentByCurrent.length > 0 && 
-                this.renderCondition(this.props.loadLocationById[0].type)} 
-              </div>
-              <TopDisp />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="dashboard-container">
+        <Directory />
+        <div className='globe'>
+        {this.props.loadLocationById.length > 0 && 
+          this.props.loadParentByCurrent.length > 0 && 
+          this.renderCondition(this.props.loadLocationById[0].type)} 
+        </div>
+        <TopDisp />
+      </div>
+    )
+  }
 }
 
 function mapStateToProps({ currentLocation, auth, loadLocationById, loadParentByCurrent }) {
-    return { currentLocation, auth, loadLocationById, loadParentByCurrent }
+  return { currentLocation, auth, loadLocationById, loadParentByCurrent }
 }
 
 function mapDispatchToProps(dispatch) {
-    return { 
-        getLocationsByParent: parentId => dispatch(getLocationsByParent(parentId)),
-        updateCurrentLocation: id => dispatch(updateCurrentLocation(id)),
-        getTopContributors: () => dispatch(getTopContributors())
-    }
+  return { 
+    getLocationsByParent: parentId => dispatch(getLocationsByParent(parentId)),
+    updateCurrentLocation: id => dispatch(updateCurrentLocation(id)),
+    getTopContributors: () => dispatch(getTopContributors())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
