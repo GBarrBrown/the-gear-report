d534788fd988ec1ba876863bb6c9b4b6f2a3214a
Richard Graham <richgrahamdev@gmail.com>
2019-03-07 00:37:48 +1300

    deleted seed files in tickets

--- migrations/20190228205533_tickets.js
+++ migrations/20190228205533_tickets.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('tickets', (table) => {
     table.increments('id').primary()
     table.string('title').notNullable();
-    table.text('description').notNullable();
+    table.text('description');
     table.integer('severity').notNullable();
     table.string('img')
     table.boolean('has_grant').defaultTo(false)
